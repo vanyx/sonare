88bc937193a553c6a41b5cad0d4fd81264b8ec7e
vanyxx <benalouanet@gmail.com>
2024-10-24 13:37:36 +0200

    azimut from flutter map math

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart'; // Pour utiliser LatLng
+import 'dart:math';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class TestPage extends StatefulWidget {
   @override
@@ -9,29 +11,229 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
+  LatLng? _currentPosition;
+
+  MapController _mapController = MapController();
+
+  StreamSubscription<Position>? _positionSubscription;
+
+  double _zoomLevel = 15.0;
+
+  double _sizeScreenCoef = 0.9; //min 0.0 et max 1.0
+
+  double _redAngle = 0; // angle du cercle rouge en degre
+
+  double _blueThickness = 10; //epaisseur cercle bleu
+
+  double _redThickness = 20; //diametre cercle rouge
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  void _onMapReady() {
+    _listeningToLocationChanges();
+    updateRedCircleAngle();
+  }
+
+  @override
+  void dispose() {
+    _positionSubscription?.cancel(); // Annuler le stream de géolocalisation
+    super.dispose();
+  }
+
+  /**
+   * Trouvé depuis le package Flutter 'flutter_map_math'
+   * 
+   * https://github.com/Ujjwalsharma2210/flutter_map_math/blob/main/lib/flutter_geo_math.dart
+   */
+  double bearingBetween(double lat1, double lon1, double lat2, double lon2) {
+    var dLon = degreesToRadians(lon2 - lon1);
+    var y = sin(dLon) * cos(degreesToRadians(lat2));
+    var x = cos(degreesToRadians(lat1)) * sin(degreesToRadians(lat2)) -
+        sin(degreesToRadians(lat1)) * cos(degreesToRadians(lat2)) * cos(dLon);
+    var angle = atan2(y, x);
+    return (radiansToDegrees(angle) + 360) % 360;
+  }
+
+  /// Convert degrees to radians
+  double degreesToRadians(double degrees) {
+    return degrees * pi / 180;
+  }
+
+  /// converts radians to degrees
+  double radiansToDegrees(double radians) {
+    return radians * 180 / pi;
+  }
+
+  Future<void> _getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    if (mounted) {
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+      });
+    }
+  }
+
+  void _listeningToLocationChanges() {
+    _positionSubscription = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+      ),
+    ).listen((Position position) {
+      if (mounted) {
+        LatLng newPosition = LatLng(position.latitude, position.longitude);
+        setState(() {
+          _currentPosition = newPosition;
+        });
+      }
+    });
+  }
+
+  void updateRedCircleAngle() {
+    LatLng targetPosition = LatLng(47.744281802357015, -3.3593437166432123);
+
+    if (_currentPosition != null) {
+      final double _redAngleDegrees = bearingBetween(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude,
+          targetPosition.latitude,
+          targetPosition.longitude);
+
+      final double newAngle = _redAngleDegrees * (pi / 180);
+
+      if (mounted) {
+        // Vérification si le widget est toujours monté
+        setState(() {
+          _redAngle = newAngle - pi / 2;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    final double _blueRadius =
+        (screenSize.width * _sizeScreenCoef) / 2; //rayon du cercle bleu
+
+    final Offset center = Offset(
+        screenSize.width / 2, screenSize.height / 2); //coord. centre de l'ecran
+
+    // calcul de la position actuelle du cercle rouge
+    final Offset redCirclePosition = Offset(
+      center.dx + _blueRadius * cos(_redAngle),
+      center.dy + _blueRadius * sin(_redAngle),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Minimum Example'),
-      ),
-      body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(0, 0),
-          initialZoom: 1,
-          minZoom: 0,
-          maxZoom: 19,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName:
-                'net.tlserver6y.flutter_map_location_marker.example',
-            maxZoom: 19,
-          ),
-          CurrentLocationLayer(),
-        ],
+      body: Center(
+        child: _currentPosition == null
+            ? CircularProgressIndicator()
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: screenSize.width * _sizeScreenCoef,
+                    height: screenSize.width * _sizeScreenCoef,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.transparent,
+                    ),
+                    child: ClipOval(
+                      child: FlutterMap(
+                        mapController: _mapController,
+                        options: MapOptions(
+                          initialCenter: _currentPosition!,
+                          initialZoom: _zoomLevel,
+                          minZoom: _zoomLevel, // empeche de zoomer
+                          maxZoom: _zoomLevel, // empeche de zoomer
+                          onMapReady: _onMapReady,
+                          interactionOptions: InteractionOptions(
+                            flags: 0,
+                          ),
+                        ),
+                        children: [
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            userAgentPackageName: 'com.vanyx.sonare',
+                          ),
+                          MarkerLayer(
+                            markers: [
+                              Marker(
+                                width: 25.0,
+                                height: 25.0,
+                                point: _currentPosition!,
+                                child: Icon(
+                                  Icons.navigation,
+                                  color: const Color.fromARGB(255, 197, 14, 14),
+                                  size: 30.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // cercle bleu
+                  CustomPaint(
+                    size: Size(screenSize.width, screenSize.height),
+                    painter: CirclePainter(center, _blueRadius, _blueThickness),
+                  ),
+                  // cercle rouge
+                  Positioned(
+                    left: redCirclePosition.dx -
+                        (_redThickness /
+                            2), // position du cercle (moins le rayon)
+                    top: redCirclePosition.dy -
+                        (_redThickness /
+                            2), // position du cercle (moins le rayon)
+                    child: Container(
+                      width: _redThickness,
+                      height: _redThickness,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  final Offset center;
+  final double radius;
+  final double thickness; //epaisseur bordure
+
+  CirclePainter(this.center, this.radius, this.thickness);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = const Color.fromARGB(255, 21, 66, 180)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = thickness;
+
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false; // pas besoin de redessiner constamment
   }
 }
